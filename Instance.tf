5cdca76f1a47baa7104827d5a00ae05aa1d6d839
dilshanran <drmanawadu071@gmail.com>
2025-01-16 07:40:40 +0530

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami               = var.amiID[var.region]
-  subnet_id         = "subnet-03c8233ac01e42b71" # Replace with the actual subnet ID
+  subnet_id         = "subnet-03c82" # Replace with the actual subnet ID
   instance_type     = "t3.micro"
   key_name          = aws_key_pair.dovekey.key_name
   security_groups   = [aws_security_group.dove-sg.id]
